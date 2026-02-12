f65cd252765af8d935a5a2eb7f41e2ffe5ab0606
sabreshao <sabre.shao@amd.com>
2026-02-12 12:20:40 +0000

    Minor change.

--- alloc_2.sh
+++ alloc_2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-salloc -N 2 --ntasks-per-node=1 --nodelist=node28,node29 --gres=gpu:8 -p pegasus -t 00:10:00
+salloc -N 2 --ntasks-per-node=1 --nodelist=node28,node29 --gres=gpu:8 -p pegasus -t 01:00:00
