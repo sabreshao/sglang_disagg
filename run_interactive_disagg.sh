2dc2f614dfd01ad7c6a8eeb6fcff870fc5f4a0e4
Alex Sun <alex.s@amd.com>
2026-03-06 00:41:55 -0600

    tidy up

--- run_interactive_disagg.sh
+++ run_interactive_disagg.sh
@@ -21,21 +21,21 @@
 
 export xP=1
 export yD=1
-export NUM_NODES=5
+export NUM_NODES=3
 export MODEL_NAME=DeepSeek-R1
 export MODEL_DIR="/mnt/nfs/RAID/shared/huggingface/hub/"
 export PREFILL_TP_SIZE=8
 export PREFILL_ENABLE_EP=true
 export PREFILL_ENABLE_DP=true
-export DECODE_TP_SIZE=32
+export DECODE_TP_SIZE=16
 export DECODE_ENABLE_EP=true
 export DECODE_ENABLE_DP=true
 export DECODE_MTP_SIZE=0
-export BENCH_INPUT_LEN=1024
-export BENCH_OUTPUT_LEN=1024
+export BENCH_INPUT_LEN=3300
+export BENCH_OUTPUT_LEN=400
 export BENCH_RANDOM_RANGE_RATIO=1
 export BENCH_NUM_PROMPTS_MULTIPLIER=10
-export BENCH_MAX_CONCURRENCY=32
+export BENCH_MAX_CONCURRENCY=1024
 export LOAD_DUMMY=0
 #export SGLANG_DIR=/mnt/nfs/sashao/workspace/dist_inference/sglang
 export DRY_RUN=0
