e6cc5169a73a8dce1de9f74249f5594d4e98b9a6
sabreshao <Sabre.Shao@amd.com>
2026-04-03 11:35:33 +0000

    remove all files

--- benchmark_deepseek_v32_pd_tp8_ep8.sh
+++
@@ -1,3 +0,0 @@
-bash give_me_nodes.sh
-sleep 5
-LOAD_DUMMY=0 DEFAULT_DOCKER_IMAGE=rocm/sgl-dev:v0.5.10rc0-rocm720-mi35x-20260331  PROFILE=deepseek_v32_fp4_default  BENCH_MODE=poisson BENCH_REQUEST_RATE=inf BENCH_BURSTINESS=1.0 BENCH_NUM_PROMPTS=1 BENCH_NUM_WARMUPS=1 bash run_pd_tep8.sh
